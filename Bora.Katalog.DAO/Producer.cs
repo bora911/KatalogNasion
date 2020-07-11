a9afbf2e3fbf26cb630ff6ddfe27ebcb73575975
bora911 <bora911@interia.pl>
2020-07-11 08:24:23 +0200

    Add files via upload

---
+++ Bora.Katalog.DAO/Producer.cs
@@ -0,0 +1,19 @@
+﻿using Bora.Katalog.INTERFACES;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bora.Katalog.DAO
+{
+    class Producer : IProducer
+    {
+        public int Id { get; set; }
+
+        public String Name { get; set; }
+
+        public override string ToString()
+        {
+            return  Name.ToString();
+        }
+    }
+}
---
+++ Bora.Katalog.DAO/Seed.cs
@@ -0,0 +1,37 @@
+﻿using Bora.Katalog.CORE;
+using Bora.Katalog.INTERFACES;
+using System;
+
+namespace Bora.Katalog.DAO
+{
+    public class Seed : ISeed
+    {
+        public int Id { get; set; }
+        public string Variety { get; set; }
+        
+        public string Type { get; set; }
+
+        public string Caliber { get; set; }
+
+        public string Validity { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        public DateTime ProductionDate { get; set; }
+        
+        public IProducer Producer { get; set; }
+
+        public SeedType SeedType { get; set; }
+
+        public SeedCaliber SeedCaliber { get; set; }
+
+        public ValidityTime ValidityTime { get; set; }
+
+        private string SearchField => Producer + Type + Variety ;
+        public bool Contains(string filterText)
+        {
+            return SearchField.ToLower().Replace(" ", string.Empty).Contains(filterText.ToLower().Replace(" ", string.Empty));
+        }
+
+
+    }
+}
